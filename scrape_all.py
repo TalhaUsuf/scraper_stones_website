6faf2ab8f4130580cafe5ea6439202858607ad8c
talha <talha.yousuf@stech.ai>
2022-11-01 13:54:11 +0500

    🔨 made requirements.txt file

--- scrape_all.py
+++ scrape_all.py
@@ -5,11 +5,11 @@ from pathlib import Path
 import json
 import requests
 import pandas as pd
-from tqdm import  tqdm
-
+from tqdm import tqdm
+from pqdm.threads import pqdm
 
 ##
-
+# 🔴 go to the page which has all the URLs of countries
 response = requests.get("https://www.stoneadd.com/Stone-Suppliers.html")
 # make a soup from response
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -26,6 +26,7 @@ for k in tqdm(soup.find_all("div", class_="xlei"), colour='green'):
         _URLS.append(f"{j.a.get('href')}")
 
 
+# 🔵 Iterate over each country urls, open main page and get all the urls of companies
 subs = []
 # for k in tqdm(range(len(_URLS)), colour='red'):
 response = requests.get(_URLS[0])
